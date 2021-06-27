629e03924164679f98e5eb85f08b23fd725f8425
Trecia Sekgalo <tksekgalo@gmail.com>
2021-06-27 23:47:04 +0200

    Nav bar complete

---
+++ index.js
@@ -0,0 +1,13 @@
+// toggle menu
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+  nav = document.getElementById(navId)
+
+  if(toggle && nav) {
+    toggle.addEventListener("click", ()=> {
+      nav.classList.toggle("show")
+    })
+  }
+}
+
+showMenu("toggle-menu", "nav-menu")
