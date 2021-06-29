0180171a7d1b42c026d163d836e2373e65d8e280
Trecia Sekgalo <tksekgalo@gmail.com>
2021-06-29 02:45:54 +0200

    Scroll animation complete

--- index.js
+++ index.js
@@ -11,3 +11,32 @@ const showMenu = (toggleId, navId) => {
 }
 
 showMenu("toggle-menu", "nav-menu")
+
+// scroll reveal animation
+const scroll = ScrollReveal ({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true
+})
+// home scroll
+scroll.reveal(".home-title", {})
+scroll.reveal(".home-scroll", {delay: 300})
+
+// about scroll 
+scroll.reveal(".about-img", {delay: 500})
+scroll.reveal(".about-subtitle", {delay: 300})
+scroll.reveal(".about-text", {delay: 400})
+scroll.reveal(".about-link", {delay: 500})
+
+// abilities scroll
+scroll.reveal(".skills-subtitle", {})
+scroll.reveal(".skills-name", {distance: "20px", delay: 50, interval: 100})
+scroll.reveal(".skills-img", {delay: 400})
+
+// work reveal
+scroll.reveal(".work-img", {});
+
+// contact scroll 
+scroll.reveal(".contact-container", {})
+scroll.reveal(".social-container", {delay: 300})
