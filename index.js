cad433b49c448d555d0ef7d5bfe6c7373cd13ab5
Trecia Sekgalo <tksekgalo@gmail.com>
2021-07-02 05:05:34 +0200

    Updated fonts, btns & header

--- index.js
+++ index.js
@@ -21,22 +21,22 @@ const scroll = ScrollReveal ({
 })
 // home scroll
 scroll.reveal(".home-title", {})
-scroll.reveal(".home-scroll", {delay: 300})
+scroll.reveal(".home-scroll", {delay: 250})
 
 // about scroll 
-scroll.reveal(".about-img", {delay: 500})
-scroll.reveal(".about-subtitle", {delay: 300})
-scroll.reveal(".about-text", {delay: 400})
-scroll.reveal(".about-link", {delay: 500})
+scroll.reveal(".about-img", {delay: 400})
+scroll.reveal(".about-subtitle", {delay: 250})
+scroll.reveal(".about-text", {delay: 300})
+scroll.reveal(".about-link", {delay: 400})
 
 // abilities scroll
 scroll.reveal(".skills-subtitle", {})
 scroll.reveal(".skills-name", {distance: "20px", delay: 50, interval: 100})
-scroll.reveal(".skills-img", {delay: 400})
+scroll.reveal(".skills-img", {delay: 300})
 
 // work reveal
 scroll.reveal(".work-img", {});
 
 // contact scroll 
 scroll.reveal(".contact-container", {})
-scroll.reveal(".social-container", {delay: 350})
+scroll.reveal(".social-container", {delay: 300})
